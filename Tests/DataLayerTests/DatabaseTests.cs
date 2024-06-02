97a291c6c5b17cf7556f95748ba48b02d95cbf0f
alexbud1 <alexbud.optional@gmail.com>
2024-06-02 15:46:56 +0200

    tests for datalayer added

--- Tests/DataLayerTests/DatabaseTests.cs
+++ Tests/DataLayerTests/DatabaseTests.cs
@@ -46,4 +46,219 @@ public class DatabaseTests
             }
         }
     }
+
+    [TestMethod]
+    public async Task TestSuppliers()
+    {
+        string supplierId = "1";
+        await _dataRepository.AddSupplierAsync(supplierId, "Test Name", "Test Address");
+
+        ISupplier supplier = await _dataRepository.GetSupplierAsync(supplierId);
+
+        Assert.AreEqual("Test Name", supplier.SupplierName);
+        Assert.AreEqual("Test Address", supplier.SupplierAddress);
+        Assert.AreEqual(supplierId, supplier.SupplierId);
+
+        Assert.IsTrue(await _dataRepository.GetSupplierCountAsync() == 1);
+
+        await _dataRepository.UpdateSupplierAsync(supplierId, "New Name", "New Address");
+
+        ISupplier updatedSupplier = await _dataRepository.GetSupplierAsync(supplierId);
+
+        Assert.AreEqual("New Name", updatedSupplier.SupplierName);
+        Assert.AreEqual("New Address", updatedSupplier.SupplierAddress);
+        Assert.AreEqual(supplierId, updatedSupplier.SupplierId);
+        Assert.AreNotEqual(updatedSupplier, supplier);
+
+        await _dataRepository.DeleteSupplierAsync(supplierId);
+
+        Assert.IsTrue(await _dataRepository.GetSupplierCountAsync() == 0);
+        Assert.IsNull(await _dataRepository.GetSupplierAsync(supplierId));
+    }
+
+    [TestMethod]
+    public async Task TestProducts()
+    {
+        string supplierId = "1";
+        await _dataRepository.AddSupplierAsync(supplierId, "Test Name", "Test Address");
+
+        string productId = "1";
+        await _dataRepository.AddProductAsync(productId, "Test Product", "Test Description", 10.0m, supplierId);
+
+        IProduct product = await _dataRepository.GetProductAsync(productId);
+
+        Assert.AreEqual("Test Product", product.ProductName);
+        Assert.AreEqual("Test Description", product.ProductDescription);
+        Assert.AreEqual(10.0m, product.ProductPrice);
+        Assert.AreEqual(supplierId, product.SupplierId);
+        Assert.AreEqual(productId, product.ProductId);
+
+        Assert.IsTrue(await _dataRepository.GetProductCountAsync() == 1);
+
+        await _dataRepository.UpdateProductAsync(productId, "New Product", "New Description", 20.0m, supplierId);
+
+        IProduct updatedProduct = await _dataRepository.GetProductAsync(productId);
+
+        Assert.AreEqual("New Product", updatedProduct.ProductName);
+        Assert.AreEqual("New Description", updatedProduct.ProductDescription);
+        Assert.AreEqual(20.0m, updatedProduct.ProductPrice);
+        Assert.AreEqual(supplierId, updatedProduct.SupplierId);
+        Assert.AreEqual(productId, updatedProduct.ProductId);
+        Assert.AreNotEqual(updatedProduct, product);
+
+        await _dataRepository.DeleteProductAsync(productId);
+
+        Assert.IsTrue(await _dataRepository.GetProductCountAsync() == 0);
+        Assert.IsNull(await _dataRepository.GetProductAsync(productId));
+    }
+
+    [TestMethod]
+    public async Task TestShops()
+    {
+        string shopId = "1";
+        await _dataRepository.AddShopAsync(shopId, "Test Name", "Test Address");
+
+        IShop shop = await _dataRepository.GetShopAsync(shopId);
+
+        Assert.AreEqual("Test Name", shop.ShopName);
+        Assert.AreEqual("Test Address", shop.ShopAddress);
+        Assert.AreEqual(shopId, shop.ShopId);
+
+        Assert.IsTrue(await _dataRepository.GetShopCountAsync() == 1);
+
+        await _dataRepository.UpdateShopAsync(shopId, "New Name", "New Address");
+
+        IShop updatedShop = await _dataRepository.GetShopAsync(shopId);
+
+        Assert.AreEqual("New Name", updatedShop.ShopName);
+        Assert.AreEqual("New Address", updatedShop.ShopAddress);
+        Assert.AreEqual(shopId, updatedShop.ShopId);
+        Assert.AreNotEqual(updatedShop, shop);
+
+        await _dataRepository.DeleteShopAsync(shopId);
+
+        Assert.IsTrue(await _dataRepository.GetShopCountAsync() == 0);
+        Assert.IsNull(await _dataRepository.GetShopAsync(shopId));
+    }
+
+    [TestMethod]
+    public async Task TestOrders()
+    {
+        string supplierId = "1";
+        await _dataRepository.AddSupplierAsync(supplierId, "Test Name", "Test Address");
+
+        string productId = "1";
+        await _dataRepository.AddProductAsync(productId, "Test Product", "Test Description", 10.0m, supplierId);
+
+        string shopId = "1";
+        await _dataRepository.AddShopAsync(shopId, "Test Name", "Test Address");
+
+        string orderId = "1";
+        await _dataRepository.AddOrderAsync(orderId, productId, shopId);
+
+        IOrder order = await _dataRepository.GetOrderAsync(orderId);
+
+        Assert.AreEqual(productId, order.ProductId);
+        Assert.AreEqual(shopId, order.ShopId);
+        Assert.AreEqual(orderId, order.OrderId);
+
+        Assert.IsTrue(await _dataRepository.GetOrderCountAsync() == 1);
+
+        await _dataRepository.UpdateOrderAsync(orderId, productId, shopId);
+
+        IOrder updatedOrder = await _dataRepository.GetOrderAsync(orderId);
+
+        Assert.AreEqual(productId, updatedOrder.ProductId);
+        Assert.AreEqual(shopId, updatedOrder.ShopId);
+        Assert.AreEqual(orderId, updatedOrder.OrderId);
+        Assert.AreNotEqual(updatedOrder, order);
+
+        await _dataRepository.DeleteOrderAsync(orderId);
+
+        Assert.IsTrue(await _dataRepository.GetOrderCountAsync() == 0);
+        Assert.IsNull(await _dataRepository.GetOrderAsync(orderId));
+    }
+
+    [TestMethod]
+    public async Task TestEvents()
+    {
+        string supplierId = "1";
+        await _dataRepository.AddSupplierAsync(supplierId, "Test Name", "Test Address");
+
+        string productId = "1";
+        await _dataRepository.AddProductAsync(productId, "Test Product", "Test Description", 10.0m, supplierId);
+
+        string shopId = "1";
+        await _dataRepository.AddShopAsync(shopId, "Test Name", "Test Address");
+
+        string orderId = "1";
+        await _dataRepository.AddOrderAsync(orderId, productId, shopId);
+
+        string eventId = "1";
+        await _dataRepository.AddEventAsync(eventId, "Test Description");
+
+        IEvent @event = await _dataRepository.GetEventAsync(eventId);
+
+        Assert.AreEqual(orderId, @event.OrderId);
+        Assert.AreEqual(shopId, @event.ShopId);
+        Assert.AreEqual(eventId, @event.EventId);
+
+        Assert.IsTrue(await _dataRepository.GetEventCountAsync() == 1);
+
+        await _dataRepository.UpdateEventAsync(eventId, "New Description");
+
+        IEvent updatedEvent = await _dataRepository.GetEventAsync(eventId);
+
+        Assert.AreEqual(orderId, updatedEvent.OrderId);
+        Assert.AreEqual(shopId, updatedEvent.ShopId);
+        Assert.AreEqual(eventId, updatedEvent.EventId);
+        Assert.AreNotEqual(updatedEvent, @event);
+
+        await _dataRepository.DeleteEventAsync(eventId);
+
+        Assert.IsTrue(await _dataRepository.GetEventCountAsync() == 0);
+        Assert.IsNull(await _dataRepository.GetEventAsync(eventId));
+    }
+
+    [TestMethod]
+    public async Task TestOrderStatuses()
+    {
+        string supplierId = "1";
+        await _dataRepository.AddSupplierAsync(supplierId, "Test Name", "Test Address");
+
+        string productId = "1";
+        await _dataRepository.AddProductAsync(productId, "Test Product", "Test Description", 10.0m, supplierId);
+
+        string shopId = "1";
+        await _dataRepository.AddShopAsync(shopId, "Test Name", "Test Address");
+
+        string orderId = "1";
+        await _dataRepository.AddOrderAsync(orderId, productId, shopId);
+
+        string orderStatusId = "1";
+        await _dataRepository.AddOrderStatusAsync(orderStatusId, OrderStatusEnum.Pending, orderId);
+
+        IOrderStatus orderStatus = await _dataRepository.GetOrderStatusAsync(orderStatusId);
+
+        Assert.AreEqual(OrderStatusEnum.Pending, orderStatus.Status);
+        Assert.AreEqual(orderId, orderStatus.OrderId);
+        Assert.AreEqual(orderStatusId, orderStatus.OrderStatusId);
+
+        Assert.IsTrue(await _dataRepository.GetOrderStatusCountAsync() == 1);
+
+        await _dataRepository.UpdateOrderStatusAsync(orderStatusId, OrderStatusEnum.Completed, orderId);
+
+        IOrderStatus updatedOrderStatus = await _dataRepository.GetOrderStatusAsync(orderStatusId);
+
+        Assert.AreEqual(OrderStatusEnum.Completed, updatedOrderStatus.Status);
+        Assert.AreEqual(orderId, updatedOrderStatus.OrderId);
+        Assert.AreEqual(orderStatusId, updatedOrderStatus.OrderStatusId);
+        Assert.AreNotEqual(updatedOrderStatus, orderStatus);
+
+        await _dataRepository.DeleteOrderStatusAsync(orderStatusId);
+
+        Assert.IsTrue(await _dataRepository.GetOrderStatusCountAsync() == 0);
+        Assert.IsNull(await _dataRepository.GetOrderStatusAsync(orderStatusId));
+    }
+
 }
